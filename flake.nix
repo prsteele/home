a2363e20fa0e0227e78236aa2e0954be02aee3bf
Patrick Steele <prsteele@proton.me>
2024-01-25 21:28:18 -0500

    feat: define a default flake action
    You can now rebuild with `nix run .`.

--- flake.nix
+++ flake.nix
@@ -6,20 +6,34 @@
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     {
       packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
         {
           homeConfigurations =
             {
               prsteele = home-manager.lib.homeManagerConfiguration {
-                pkgs = nixpkgs.legacyPackages.${system};
+                inherit pkgs;
                 modules = [ ./home.nix ];
               };
             };
+
+          # Convenience wrapper around rebuilding with flakes
+          switch = pkgs.writeShellApplication {
+            name = "switch";
+            runtimeInputs = [home-manager.packages.${system}.default];
+            text = ''
+            home-manager switch --flake .
+            '';
+          };
+
+          default = self.packages.${system}.switch;
         }
       );
 
